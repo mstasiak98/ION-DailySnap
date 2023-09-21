d9b21b35311529b1b284c7de41e0f7bf952fe16e
Mikołaj Stasiak <92719526+mstasiak98@users.noreply.github.com>
2023-09-21 20:33:23 +0200

    Install capacitor, create photo service, display taken-photos (#1)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {PhotoService} from "./data-access/photo.service";
+import {map} from "rxjs";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  photos$ = this.photoService.photos$.pipe(
+    map((photos) =>
+      photos.map((photo) => ({
+        ...photo,
+        safeResourceUrl: this.sanitizer.bypassSecurityTrustResourceUrl(
+          photo.path
+        ),
+      }))
+    )
+  );
+
+  constructor(public photoService: PhotoService, private sanitizer: DomSanitizer) {}
 
 }
---
+++ src/app/shared/models/photo.ts
@@ -0,0 +1,8 @@
+import {SafeResourceUrl} from "@angular/platform-browser";
+
+export interface Photo {
+  name: string;
+  path: string;
+  dateTaken: string;
+  safeResourceUrl?: SafeResourceUrl;
+}
